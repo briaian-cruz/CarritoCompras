e56d7d86ff9354f402fea2d1040c6a4dcebcc870
briaian-cruz <cruzbriaian@gmail.com>
2024-11-26 12:09:17 -0300

    cool-web

--- Carrito/src/components/Carrito.jsx
+++ Carrito/src/components/Carrito.jsx
@@ -1,11 +1,10 @@
-function Carrito({ productosCarrito }) {
+function Carrito({ productosCarrito, quitarCarrito }) {
   return (
     <div>
       <h2>Carrito</h2>
       {productosCarrito.map((producto) => (
         <p key={producto.id} onClick={() => quitarCarrito(producto)}>
-          {producto.name} ${producto.unitPrice} Cantidad:
-          {producto.cantidad}
+          {producto.name} ${producto.unitPrice} Cant: {producto.cantidad}
         </p>
       ))}
     </div>
